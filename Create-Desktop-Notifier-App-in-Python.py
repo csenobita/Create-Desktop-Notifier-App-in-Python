dc682f4cc91f646e505ef7e20ea3a2ff90d42b9e
Md Omar Hanif <162924689+csenobita@users.noreply.github.com>
2024-03-21 03:23:09 +0600

    Add files via upload

---
+++ Create-Desktop-Notifier-App-in-Python.py
@@ -0,0 +1,13 @@
+from plyer import notification
+import time
+
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title='Virus & threat protection',
+            message='LOL \n Your System Hack 😭😭😭',
+            app_icon="python-logo-only.ico",
+            timeout=5
+
+        )
+        time.sleep(10)
